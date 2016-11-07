a3b47f2bd042a3476420ee0901576244b40bb483
mvl22 <martin@lucas-smith.co.uk>
2016-11-07 13:06:27 +0000

    Add a supervision form

--- supervisionSignup.php
+++ supervisionSignup.php
@@ -34,6 +34,13 @@ class supervisionSignup extends frontControllerApplication
 	{
 		# Specify additional actions
 		$actions = array (
+			'add' => array (
+				'description' => 'Create a new supervision',
+				'url' => 'add/',
+				'tab' => 'Create a new supervision',
+				'icon' => 'add',
+				'enableIf' => $this->userIsStaff,
+			),
 		);
 		
 		# Return the actions
@@ -70,9 +77,116 @@ class supervisionSignup extends frontControllerApplication
 		# Start the HTML
 		$html = '';
 		
+		# Give link for staff
+		if ($this->userIsStaff) {
+			$html .= "\n<h2>Create supervision signup sheet</h2>";
+			$html .= "\n<p>As a member of staff, you can <a href=\"{$this->baseUrl}/add/\" class=\"actions\"><img src=\"/images/icons/add.png\" alt=\"Add\" border=\"0\" /> create a supervision signup sheet</a>.</p>";
+		}
 		
 		# Return the HTML
 		echo $html;
 	}
+	
+	
+	# Function to create a new supervision
+	public function add ()
+	{
+		# Start the HTML
+		$html = '';
+		
+		# Show the form
+		$html .= $this->supervisionForm ();
+		
+		# Return the HTML
+		echo $html;
+	}
+	
+	
+	# Supervision editing form
+	private function supervisionForm ()
+	{
+		# Start the HTML
+		$html = '';
+		
+		# Get the courses from the callback
+		$coursesCallback = $this->settings['coursesCallback'];
+		$courses = $coursesCallback ();
+		
+		# Databind a form
+		$form = new form (array (
+			'displayRestrictions' => false,
+			'formCompleteText' => false,
+			'databaseConnection' => $this->databaseConnection,
+			'richtextEditorToolbarSet' => 'Basic',
+			'richtextWidth' => 650,
+			'richtextHeight' => 250,
+			'cols' => 80,
+			'autofocus' => true,
+			'unsavedDataProtection' => true,
+		));
+		$form->dataBinding (array (
+			'database' => $this->settings['database'],
+			'table' => $this->settings['table'],
+			'intelligence' => true,
+			'exclude' => array ('username', 'courseName'),		// Fixed data fields, handled below
+			'attributes' => array (
+				'courseId' => array ('heading' => array (3 => 'Supervision details'), 'type' => 'select', 'values' => $courses, ),
+				'studentsPerTimeslot' => array ('heading' => array (3 => 'Dates, times and locations'), 'type' => 'number', ),	#!# Shouldn't need to specify type - is an ultimateForm bug
+			),
+		));
+		$form->input (array (
+			'name' => 'timeslots',
+			'title' => 'Timeslots (start time of each available supervision)',
+			'expandable' => "\n",
+			'required' => true,
+		));
+		if ($result = $form->process ($html)) {
+			
+			# Add in fixed data
+			$result['username'] = $this->user;
+			$result['updatedAt'] = 'NOW()';
+			
+			# Add in the course name so that this not dependent on a live feed which may change from year to year
+			$coursesFlattened = application::flattenMultidimensionalArray ($courses);
+			$result['courseName'] = $coursesFlattened[$result['courseId']];
+			
+			# Extract the timeslots for entering in the separate timeslot table
+			$timeslots = explode ("\n", $result['timeslots']);
+			unset ($result['timeslots']);
+			
+			# Insert the new supervision into the database
+			if (!$this->databaseConnection->insert ($this->settings['database'], $this->settings['table'], $result)) {
+				$this->throwError ('There was a problem creating the new supervision signup sheet.', false, application::dumpData ($this->databaseConnection->error (), false, true));
+				echo $html;
+				return false;
+			}
+			
+			# Get the supervision ID just inserted
+			$supervisionId = $this->databaseConnection->getLatestId ();
+			
+			# Unique the list of timeslots
+			$timeslots = array_unique ($timeslots);
+			
+			# Add each timeslot
+			$timeslotInserts = array ();
+			foreach ($timeslots as $index => $timeslot) {
+				$timeslotInserts[] = array (
+					'supervisionId' => $supervisionId,
+					'startTime' => $timeslot,
+				);
+			}
+			
+			# Insert the new timeslots
+			if (!$this->databaseConnection->insertMany ($this->settings['database'], 'timeslots', $timeslotInserts)) {
+				$html .= '<p class="warning">There was a problem creating the new supervision signup sheet.</p>';
+				return false;
+			}
+		}
+		
+		# Return the HTML
+		return $html;
+	}
+	
+}
 
 ?>
